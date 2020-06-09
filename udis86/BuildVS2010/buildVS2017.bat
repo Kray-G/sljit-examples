9f2ac0310b5566b5ed80527fb9b54f69c7e9d52f
Kray-G <nagai.kazuya@gmail.com>
2020-06-09 09:10:39 +0900

    updated disasm.

--- udis86/BuildVS2010/buildVS2017.bat
+++ udis86/BuildVS2010/buildVS2017.bat
@@ -4,4 +4,4 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86_
 msbuild.exe udis86.sln /t:Clean
 msbuild.exe build.proj /t:Clean,BuildRelease_x86,BuildRelease_x64,PostBuild
 REM msbuild.exe build.proj /t:Clean,BuildRelease_x86,BuildRelease_x64,BuildDebug_x86,BuildDebug_x64,PostBuild
-pause
+REM pause
